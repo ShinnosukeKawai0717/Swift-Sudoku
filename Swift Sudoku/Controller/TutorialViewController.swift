5f6eeeef32b4bb58336b8229c19f05e2b4391e58
shinnosuke kawai <shinnosuke.kawai@sjsu.edu>
2022-07-23 00:48:34 +0900

    new VC added

---
+++ Swift Sudoku/Controller/TutorialViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TutorialViewController.swift
+//  Swift Sudoku
+//
+//  Created by Shinnosuke Kawai on 7/23/22.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
